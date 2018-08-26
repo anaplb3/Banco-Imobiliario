47def9ff24a80029b5d33347c09966d03ae7ac4f
anaplb3 <ana.paula@dce.ufpb.br>
2018-08-26 16:11:06 -0300

    Parada Livre foi adicionado ao tabuleiro

--- tabuleiro/Tabuleiro.java
+++ tabuleiro/Tabuleiro.java
@@ -6,25 +6,13 @@ import java.util.LinkedList;
 import posicoes.Companhia;
 import posicoes.Imposto;
 import posicoes.Lucros;
+import posicoes.ParadaLivre;
 import posicoes.Posicao;
 import posicoes.Prisao;
 import posicoes.Propriedade;
 import posicoes.SorteOuReves;
 
 public class Tabuleiro {
-
-	public void checandoNomesEPosicao() {
-	    LinkedList<Posicao> tab = criandoTab();
-
-	    for (int i = 0; i < tab.size(); i++) {
-	      if (tab.get(i) == null) {
-	        System.out.println("nulo");
-	      } else {
-	        System.out.println("Posicao: " + i);
-	        tab.get(i).getNomeDaPosicao();
-	      }
-	    }
-	  }
 
 	public LinkedList<Posicao> criandoTab() {
 		LinkedList<Posicao> tab = new LinkedList<>();
@@ -33,10 +21,11 @@ public class Tabuleiro {
 		SorteOuReves sorte = new SorteOuReves();
 		Lucros lucros = new Lucros();
 		Imposto imposto = new Imposto();
+		ParadaLivre parada = new ParadaLivre();
 
 		int contador = 0;
 		for (int i = 0; i < 40; i++) {
-			
+
 			if (i == 2 || i == 12 || i == 16 || i == 22 || i == 27 || i == 37) {
 				tab.add(sorte);
 			} else if (i == 10 || i == 30) {
@@ -48,32 +37,34 @@ public class Tabuleiro {
 			} else if (i == 5) {
 				Companhia companhia1 = new Companhia("Cia Ferroviaria", 200, 50);
 				tab.add(companhia1);
-				
+
 			} else if (i == 7) {
-				
+
 				Companhia companhia2 = new Companhia("Cia de Viação", 200, 50);
 				tab.add(companhia2);
-				
+
 			} else if (i == 15) {
 				Companhia companhia3 = new Companhia("Cia de Taxi", 150, 40);
 				tab.add(companhia3);
-				
+
 			} else if (i == 25) {
 				Companhia companhia4 = new Companhia("Cia de Navegação", 150, 40);
 				tab.add(companhia4);
-				
+
 			} else if (i == 32) {
 				Companhia companhia5 = new Companhia("Cia de Aviação", 200, 50);
 				tab.add(companhia5);
-				
+
 			} else if (i == 35) {
 				Companhia companhia6 = new Companhia("Cia de Taxi Áereo", 200, 50);
 				tab.add(companhia6);
 			}
 
-			else if (i == 0 || i == 20) {
+			else if (i == 0) {
 				tab.add(null);
-				
+
+			} else if (i == 20) {
+				tab.add(parada);
 			} else {
 				tab.add(propriedades.get(contador));
 				contador += 1;
@@ -90,7 +81,6 @@ public class Tabuleiro {
 		ArrayList<Integer> precos = precoDasPropriedades();
 		ArrayList<Integer> aluguel = alugueis();
 		ArrayList<Propriedade> propriedades = new ArrayList<>();
-		
 
 		for (int i = 0; i < nomes.size(); i++) {
 			Propriedade p = new Propriedade();
@@ -120,10 +110,7 @@ public class Tabuleiro {
 			propriedades.add(p);
 		}
 
-		
-	
-
-	return propriedades;
+		return propriedades;
 
 	}
 
